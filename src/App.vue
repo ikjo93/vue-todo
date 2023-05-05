e69cd9e5533def4327a65af9e9ed6d2b6e9c91c7
ikjo93 <auddlr100@naver.com>
2023-05-06 00:47:21 +0900

    feat : TODO 리스트 생성/조회/삭제/ 기능 구현

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
     <TodoHeader></TodoHeader>
-    <TodoInput></TodoInput>
-    <TodoList></TodoList>
-    <TodoFooter></TodoFooter>
+    <TodoInput v-on:addTodoItem="addOneItem"></TodoInput>
+    <TodoList v-bind:propsdata="todoItems" v-on:removeTodoItem="removeOneItem" v-on:toggleTodoItem="toggleOneItem"></TodoList>
+    <TodoFooter v-on:clearAllTodoItems="clearAllItems"></TodoFooter>
   </div>
 </template>
 
@@ -15,6 +15,39 @@ import TodoList from './components/TodoList.vue';
 import TodoFooter from './components/TodoFooter.vue';
 
 export default {
+  data() {
+    return {
+      todoItems: []
+    }
+  },
+  methods: {
+    addOneItem: function(todoItem) {
+      let obj = { completed: false, item: todoItem };
+      localStorage.setItem(todoItem, JSON.stringify(obj));
+      this.todoItems.push(obj);
+    },
+    removeOneItem: function(todoItem, index) {
+      localStorage.removeItem(todoItem.item);
+      this.todoItems.splice(index, 1);
+    },
+    toggleOneItem: function (todoItem, index) {
+      this.todoItems[index].completed = !this.todoItems[index].completed;
+      
+      localStorage.removeItem(todoItem.item);
+      localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
+    },
+    clearAllItems: function() {
+      localStorage.clear();
+      this.todoItems = [];
+    }
+  },
+  created: function () {
+    if (localStorage.length > 0) {
+      for (let i = 0; i < localStorage.length; i++) {
+        this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+      }
+    }
+  },
   components: {
     'TodoHeader': TodoHeader,
     'TodoInput': TodoInput,
@@ -25,5 +58,18 @@ export default {
 </script>
 
 <style>
-
+  body {
+    text-align: center;
+    background-color: #F6F6F6;
+  }
+  input {
+    border-style: groove;
+    width: 200px;
+  }
+  button {
+    border-style: groove;
+  }
+  .shadow {
+    box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
+  }
 </style>
--- src/components/TodoFooter.vue
+++ src/components/TodoFooter.vue
@@ -1,15 +1,30 @@
 <template>
-  <div>
-    
+  <div class="clearAllContainer">
+    <span class="clearAllBtn" v-on:click="clearTodos">Clear All</span>
   </div>
 </template>
 
 <script>
 export default {
-
+  methods: {
+    clearTodos: function() {
+      this.$emit('clearAllTodoItems');
+    }
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+  .clearAllContainer {
+    width: 8.5rem;
+    height: 50px;
+    line-height: 50px;
+    background-color: white;
+    border-radius: 5px;
+    margin: 0 auto;
+  }
+  .clearAllBtn {
+    color: #e20303;
+    display: block;
+  }
 </style>
